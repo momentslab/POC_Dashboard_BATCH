d75d67988f268b87c6383173ab59868b747fae21
Enzo Dupont <enzodupont@mac.home>
2025-11-21 14:39:51 +0100

    Ajout du fichier py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Affiche un titre
+st.title("Mon premier dashboard")
+
+# Affiche une phrase simple
+st.write("Bonjour ! Voici ma première phrase affichée avec Streamlit.")
